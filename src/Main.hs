c2c372ac4ad7d57f95cabe6430188d864d424519
Chris S <chris@soundshare.co.uk>
2024-08-21 21:11:11 +0100

    github action to replicate

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,16 @@ module Main where
 
 import Web.Scotty
 import Control.Monad.IO.Class
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
   scotty 3000 $ do
-    get "/test" $ do
-      error "test"
+    get "/normal" $ liftIO $ do
+      print "normal"
+    get "/test" $ liftIO $ do
+      print "pre error"
+      error "THE ERROR"
+      print "post error"
